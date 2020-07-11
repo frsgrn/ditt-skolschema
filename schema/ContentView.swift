abb57ea17a8ffacd0faa92930988425db8680dd3
Victor Forsgren <forsgren.victor@gmail.com>
2020-07-11 22:33:52 +0200

    Initial Commit

---
+++ schema/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  schema
+//
+//  Created by Victor Forsgren on 2020-07-11.
+//  Copyright © 2020 Victor Forsgren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
