63c95bad2967a6985cc48ad393cf6664d61097b8
Victor Forsgren <forsgren.victor@gmail.com>
2021-05-13 20:46:39 +0200

    swipe

--- schema/Views/SchemaView.swift
+++ schema/Views/SchemaView.swift
@@ -228,30 +228,144 @@ struct TodayView: View {
     @EnvironmentObject var todayController: TodayController
     @State private var birthDate = Date()
     
+    @State private var dragAmount = CGSize.zero
+    
+    
     var body: some View {
         NavigationView {
-            VStack(spacing: 0) {
-                VStack {
-                    HorizontalDatePicker(dateNumberLimit: 14).padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+            ZStack {
+            //VStack(spacing: 0) {
+                Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all)
+                ScrollView {
                     VStack {
-                        Text("\(todayController.assistantMessage())")
-                            .font(.system(size: 20)).bold().multilineTextAlignment(.center)
-                        if (!Calendar.current.isDate(todayController.selectedDate, inSameDayAs:todayController.currentDate)) {
+                        VStack {
                             HStack {
-                                Button(action: {
-                                    todayController.selectedDate = todayController.currentDate
-                                    todayController.load(ProfileManager.getSelectedProfile())
-                                }) {
-                                    Text("Du tittar just nu in i framtiden, gå tillbaka till idag?").font(.footnote)
+                                Text("\(todayController.assistantMessage())").font(.system(size: 20)).bold()
+                                Spacer()
+                            }
+                            if (!Calendar.current.isDate(todayController.selectedDate, inSameDayAs:todayController.currentDate)) {
+                                HStack {
+                                    Button(action: {
+                                        todayController.selectedDate = todayController.currentDate
+                                        todayController.load(ProfileManager.getSelectedProfile())
+                                    }) {
+                                        Text("Det här är inte idag, gå tillbaka?").font(.footnote)
+                                    }
+                                    Spacer()
+                                }.padding(EdgeInsets(top: 2, leading: 0, bottom: 0, trailing: 0))
+                            }
+                        }.padding().padding(EdgeInsets(top: 10, leading: 0, bottom: 5, trailing: 0))
+
+                        if (self.todayController.getTimetableObjectLoadFromDate(date: todayController.selectedDate) != nil) {
+                            if (self.todayController.getSelectedEventList().count > 0) {
+                                SchemaCardStack()
+                            }
+                            else if (self.todayController.getTimetableObjectLoadFromDate(date: todayController.selectedDate)!.fetchError != nil) {
+                                Text(self.todayController.getTimetableObjectLoadFromDate(date: todayController.selectedDate)!.fetchError!.message).foregroundColor(Color(UIColor.red)).padding()
+                                Spacer()
+                            }
+                            else {
+                                HStack {
+                                    Image("yoda").resizable().aspectRatio(contentMode: .fit).frame(width: 250).cornerRadius(20).padding()
+                                    Spacer()
                                 }
-                            }.padding(.top, 5)
+                            }
                         }
-                    }.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-                }.padding(EdgeInsets(top: 0, leading: 0, bottom: 13, trailing: 0))//.background(Color("DatePickerBackground"))
-                VStack {
+                        
+                    }.animation(.none)
+                }
+                .offset(x: self.dragAmount.width)
+                //.animation(Animation.spring())
+                // .transition(AnyTransition.slide)
+                .simultaneousGesture(DragGesture(minimumDistance: 10, coordinateSpace: .local)
+                            .onChanged {
+                                self.dragAmount = $0.translation
+                                
+                            }
+                            .onEnded({ value in
+                                withAnimation(.spring()) {
+                                    let impactMed = UIImpactFeedbackGenerator(style: .light)
+                                        
+                                self.dragAmount = .zero
+                                if value.translation.width < -100 {
+                                    // left
+                                    print("left")
+                                    var dateComponent = DateComponents()
+                                    dateComponent.day = 1
+                                    let futureDate = Calendar.current.date(byAdding: dateComponent, to: self.todayController.selectedDate)
+                                    self.todayController.selectedDate = futureDate!
+                                    self.todayController.load(ProfileManager.getSelectedProfile())
+                                    impactMed.impactOccurred()
+                                }
+
+                                if value.translation.width > 100 {
+                                    // right
+                                    print("right")
+                                    //self.todayController.$currentDate = Date.
+                                    var dateComponent = DateComponents()
+                                    dateComponent.day = -1
+                                    let futureDate = Calendar.current.date(byAdding: dateComponent, to: self.todayController.selectedDate)
+                                    self.todayController.selectedDate = futureDate!
+                                    self.todayController.load(ProfileManager.getSelectedProfile())
+                                    impactMed.impactOccurred()
+                                }
+                                if value.translation.height < 0 {
+                                    // up
+                                }
+
+                                if value.translation.height > 0 {
+                                    // down
+                                }
+                                }
+                            }))//.animation(Animation.spring(), value: dragAmount)
+                
+                .navigationBarTitle(Text("Skoldagen"/*self.todayController.selectedDate.dayDate().capitalizingFirstLetter()*/), displayMode: .inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        /*Button(action: {
+                            self.todayController.timetableObjectLoads = []
+                            //self.todayController.load(profile: self.profiles.first(where: {$0.id!.uuidString == UserDefaults.standard.string(forKey: "selectedProfileId")}))
+                            self.todayController.load(ProfileManager.getSelectedProfile())
+                        }) {
+                            Image(systemName: "arrow.clockwise.circle.fill")
+                        }*/
+                        HorizontalDatePicker(dateNumberLimit: 1)
+                    }
+                }
+                //.background(Color("DatePickerBackground"))
+                /*VStack*/ /*ScrollView {
                     if (self.todayController.getTimetableObjectLoadFromDate(date: todayController.selectedDate) != nil) {
                         if (self.todayController.getSelectedEventList().count > 0) {
                             ScrollView {
+                                VStack {
+                                    HStack {
+                                        Text("\(todayController.assistantMessage())").font(.system(size: 20)).bold()
+                                        Spacer()
+                                    }
+                                }
+                                //VStack {
+                                    //HorizontalDatePicker(dateNumberLimit: 14).padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                                    /*VStack {
+                                        HStack {
+                                            VStack {
+                                        Text("\(todayController.assistantMessage())").font(.system(size: 20)).bold()
+                                                if (!Calendar.current.isDate(todayController.selectedDate, inSameDayAs:todayController.currentDate)) {
+                                                    HStack {
+                                                        Button(action: {
+                                                            todayController.selectedDate = todayController.currentDate
+                                                            todayController.load(ProfileManager.getSelectedProfile())
+                                                        }) {
+                                                            Text("Du tittar just nu in i framtiden, gå tillbaka till idag?").font(.footnote)
+                                                        }
+                                                        Spacer()
+                                                    }.padding(EdgeInsets(top: 3, leading: 6, bottom: 0, trailing: 0))
+                                                }
+                                            }
+                                            Spacer()
+                                        }.padding(EdgeInsets(top: 12, leading: 0, bottom: 0, trailing: 0))
+                                        
+                                    }.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+                                }.padding(EdgeInsets(top: 13, leading: 0, bottom: 10, trailing: 0))*/
                                 SchemaCardStack().padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
                             }
                         }
@@ -279,22 +393,12 @@ struct TodayView: View {
                         }
                         Spacer()
                     }
-                }
+                }*/
                 
-            }.background(Color(UIColor.systemGroupedBackground)).navigationBarTitle(Text(self.todayController.selectedDate.dayDate().capitalizingFirstLetter()), displayMode: .inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        self.todayController.timetableObjectLoads = []
-                        //self.todayController.load(profile: self.profiles.first(where: {$0.id!.uuidString == UserDefaults.standard.string(forKey: "selectedProfileId")}))
-                        self.todayController.load(ProfileManager.getSelectedProfile())
-                    }) {
-                        Image(systemName: "arrow.clockwise.circle.fill")
-                    }
-                }
-            }
+            //}
             //.background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
-        }.navigationViewStyle(StackNavigationViewStyle())
+            }
+        }//.background(Color(UIColor.systemGroupedBackground))//.navigationViewStyle(StackNavigationStyle())
     }
 }
 
@@ -333,7 +437,7 @@ struct HorizontalDatePicker : View {
     func getCircleBackgroundColor(index: Int) -> Color {
         if (self.isOnSameDay(date: self.todayController.selectedDate, date2: self.addDaysToCurrentDate(num: index))) {
             if (index != 0) {
-                return Color(UIColor.label)
+                return Color(UIColor.systemGray3)
             }
             else {
                 return Color(UIColor.systemBlue)
@@ -346,7 +450,19 @@ struct HorizontalDatePicker : View {
     
     func getDateForegroundColor(index: Int) -> Color {
         if (self.isOnSameDay(date: self.todayController.selectedDate, date2: self.addDaysToCurrentDate(num: index))) {
-            return Color(UIColor.systemBackground)
+            return Color(UIColor.label)
+        }
+        else if (index == 0) {
+            return Color(UIColor.systemBlue)
+        }
+        else {
+            return Color(UIColor.label)
+        }
+    }
+    
+    func getDayForegroundColor(index: Int) -> Color {
+        if (self.isOnSameDay(date: self.todayController.selectedDate, date2: self.addDaysToCurrentDate(num: index))) {
+            return Color(UIColor.label)
         }
         else if (index == 0) {
             return Color(UIColor.systemBlue)
@@ -360,16 +476,34 @@ struct HorizontalDatePicker : View {
         return self.isOnSameDay(date: self.todayController.selectedDate, date2: self.addDaysToCurrentDate(num: index))
     }
     
+    func getBinding () -> Binding<Date> {
+            return Binding(
+                get: { [self] in
+                    (self.todayController.selectedDate)
+                },
+                set: { [self] in
+                    self.todayController.selectedDate = $0
+                    self.todayController.load(ProfileManager.getSelectedProfile())
+                }
+            )
+    }
+    
+    
     var body: some View {
-        return ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
+        
+        return //ScrollView(.horizontal, showsIndicators: false) {
+            DatePicker(
+                    "",
+                selection: getBinding(),
+                displayedComponents: [.date]
+                )
+            /*HStack {
                 ForEach(0..<dateNumberLimit) { index in
                     Button(action: {
                         self.todayController.selectedDate = self.addDaysToCurrentDate(num: index)
-                        //self.todayController.load(profile: self.profiles.first(where: {$0.id!.uuidString == UserDefaults.standard.string(forKey: "selectedProfileId")}))
                         self.todayController.load(ProfileManager.getSelectedProfile())
                     }) {
-                        VStack {
+                        /*VStack {
                             Text(self.getDayNameOfDate(date: self.addDaysToCurrentDate(num: index)))
                                 .font(.caption)
                                 .frame(width: 40)
@@ -381,11 +515,16 @@ struct HorizontalDatePicker : View {
                                 .background(self.getCircleBackgroundColor(index: index))
                                 .foregroundColor(self.getDateForegroundColor(index: index))
                                 .clipShape(Circle())
-                        }.padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                        }.padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))*/
+                        HStack {
+                            Text(self.getDayNameOfDate(date: self.addDaysToCurrentDate(num: index))).foregroundColor(self.getDayForegroundColor(index: index))
+                                Text(self.getDayOfDate(date: self.addDaysToCurrentDate(num: index))).foregroundColor(self.getDateForegroundColor(index: index))
+                            
+                        }.padding(7).background(self.getCircleBackgroundColor(index: index)).cornerRadius(5)
                     }
                 }
-            }.padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
-        }
+            }.padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))*/
+        //}
     }
 }
 
